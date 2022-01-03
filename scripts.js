e6945051b51d8a36ce9e1925b8413bff0f5f28eb
stepanyanhayk <haykstepanyan02@gmail.com>
2022-01-03 13:30:15 +0400

    multiple modals

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
-var footballModal = document.getElementById("footballModal");
-
-var footballButton = document.getElementById("footballButton");
-
-var span = document.getElementsByClassName("close")[0];
+var buttons = document.querySelectorAll("button.modal-button");
+var modals = document.querySelectorAll(".modal");
+var spans = document.getElementsByClassName("close");
 
 
-footballButton.onclick = function() {
-    footballModal.style.display = "block";
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function(e) {
+        e.preventDefault();
+        modal = document.querySelector(e.target.getAttribute("href"));
+        modal.style.display = "block";
+    }
 }
-span.onclick = function() {
-    footballModal.style.display = "none";
+
+for (var i = 0; i < spans.length; i++) {
+    spans[i].onclick = function() {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+        }
+    }
 }
 
 window.onclick = function(event) {
-    if (event.target == footballModal) {
-        footballModal.style.display = "none";
+    if (event.target.classList.contains('modal')) {
+        for (var index in modals) {
+            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+        }
     }
 }
